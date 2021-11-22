4e63820f4f707440d597060fe0be7bd6f8c29f79
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-22 15:14:53 +0530

    Remove unused import

--- src/main/java/in/college/service/BookServiceImpl.java
+++ src/main/java/in/college/service/BookServiceImpl.java
@@ -9,7 +9,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import in.college.entities.Book;
